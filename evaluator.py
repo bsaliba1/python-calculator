7dd3a521f26db4dd1df8f1f8781daa43f5de4c1e
bsaliba <baptsal@gmail.com>
2021-03-01 00:07:03 -0500

    Modified evaluator to be able to do simple addition

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,32 @@
+import token_types
+
 class Evaluator:
     def __init__(self, tokens):
-        self.position = 0
+        self.pos = -1
         self.tokens = tokens
+        self.current_token = None
+
+    def get_next_token(self):
+        self.pos += 1
+        return self.tokens[self.pos]
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token()
+
+        left = self.current_token
+        self.eat(token_types.INTEGER)
+
+        op = self.current_token
+        self.eat(token_types.PLUS)
+
+        right = self.current_token
+
+        result = left.value + right.value
+
+        return result
